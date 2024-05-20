f1f5a8726889ac7ef79972c5c19e965f78b198aa
Armruo <armruo@outlook.om>
2024-05-20 15:04:12 +0800

    异步编程note

---
+++ initial-project/course-notes/rust-course/e1_async_await.rs
@@ -0,0 +1,37 @@
+use futures::executor::block_on;
+
+fn get_two_sites() {
+    // 创建两个新线程执行任务
+    let thread_one = thread::spawn(|| download("https://course.rs"));
+    let thread_two = thread::spawn(|| download("https://fancy.rs"));
+
+    // 等待两个线程的完成
+    thread_one.join().expect("thread one panicked");
+    thread_two.join().expect("thread two panicked");
+}
+
+async fn get_two_sites_async() {
+    // 创建两个不同的`future`，你可以把`future`理解为未来某个时刻会被执行的计划任务
+    // 当两个`future`被同时执行后，它们将并发的去下载目标页面
+    let future_one = download_async("https://www.foo.com");
+    let future_two = download_async("https://www.bar.com");
+
+    // 同时运行两个`future`，直至完成
+    join!(future_one, future_two);
+}
+
+
+// ----------------------- .await ------------------------
+
+async fn hello_world() {
+    hello_cat().await;
+    println!("hello, world!");
+}
+
+async fn hello_cat() {
+    println!("hello, kitty!");
+}
+fn main() {
+    let future = hello_world();
+    block_on(future);
+}
